462dc76a99d24cb04f603865088177f9c91de94d
ViliusKk <vilius.kuokstas@gmail.com>
2024-10-22 19:10:07 +0300

    Game is working

--- Assets/Target.cs
+++ Assets/Target.cs
@@ -7,10 +7,23 @@ public class Target : MonoBehaviour
 {
     public TMP_Text score;
     int scoreCount = 0;
+    Vector2 pos;
+
+    void Start()
+    {
+        pos = new Vector2(0, 0);
+        //pos.z = 0;
+
+        //transform.position = pos;
+    }
 
     void OnMouseDown()
     {
         score.text = scoreCount.ToString();
         scoreCount++;
+        float randomX = Random.Range(-8f, 8f);
+        float randomY = Random.Range(-4.2f, 4.2f);
+        pos = new Vector2(randomX, randomY);
+        transform.position = pos;
     }
 }
